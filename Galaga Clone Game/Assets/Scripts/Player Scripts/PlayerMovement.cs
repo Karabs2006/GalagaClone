47c9980833b1803bcfe22846de10cc5d9fa0a136
Kayy <157222013+Karabs2006@users.noreply.github.com>
2026-07-25 13:24:41 +0200

    Player Movement + Shooting
    - Added scripting to allow player to move horizontally
    - Added simple script that allows player to shoot small orbs that destroy over time

---
+++ Galaga Clone Game/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    public GameObject currentBulletPrefab;
+
+    float horizontalMovement;
+
+    public Transform gunPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+    }
+
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+
+        if (!context.performed)
+        return;
+
+
+        if (currentBulletPrefab != null && gunPoint != null)
+    {
+        GameObject bullet = Instantiate(currentBulletPrefab, gunPoint.position, gunPoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.linearVelocity = gunPoint.up * 10f;
+            Destroy(bullet, 3f);
+        }
+    }
+
+    }
+}
